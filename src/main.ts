f4696acfafe41cdb826f0d13650b3aeeec88c527
Jason Chiu <jason@runloop.ai>
2026-05-04 14:52:31 -0700

    fix: use octokit.rest.checks API and upgrade @actions/github to v6
    The old code used octokit.checks.* which is only available with
    @octokit/plugin-rest-endpoint-methods v3 (used by @actions/github v3).
    Upgrade to @actions/github v6 which uses plugin v10 where REST methods
    are under octokit.rest.* namespace. Also remove unused @octokit/rest dep.

--- src/main.ts
+++ src/main.ts
@@ -71,8 +71,8 @@ async function createCheck(
     ref: sha
   }
 
-  const res = await octokit.checks.listForRef(req)
-  const existingCheckRun = res?.data?.check_runs?.find(
+  const res = await octokit.rest.checks.listForRef(req)
+  const existingCheckRun = res.data.check_runs.find(
     check => check.name === name
   )
 
@@ -90,7 +90,7 @@ async function createCheck(
       }
     }
 
-    await octokit.checks.create(createRequest)
+    await octokit.rest.checks.create(createRequest)
   } else {
     const check_run_id = existingCheckRun.id
 
@@ -106,7 +106,7 @@ async function createCheck(
       }
     }
 
-    await octokit.checks.update(update_req)
+    await octokit.rest.checks.update(update_req)
   }
 }
 
